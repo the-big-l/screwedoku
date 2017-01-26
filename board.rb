6eb4d228f293639b6a29d2c69887f87c908b9d7f
App Academy Student <student@appacademy.io>
2017-01-26 14:42:32 -0800

    remove debugger

--- board.rb
+++ board.rb
@@ -60,7 +60,6 @@ class Board
     rows.all? { |row| solved_set?(row) } &&
       columns.all? { |col| solved_set?(col) } &&
       squares.all? { |square| solved_set?(square) }
-      debugger
   end
 
   def solved_set?(tiles)
